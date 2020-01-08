036a078dc3a5d0a5fdbaebd3fa36f8903d349590
Elissa <elissa.matsushita@gmail.com>
2020-01-08 02:53:44 +0000

    eslint

--- d2-focal/password.js
+++ d2-focal/password.js
@@ -1,32 +1,28 @@
-/*
+const args = process.argv.slice(2);
 
-*/
-
-const arguments = process.argv.slice(2);
-
-const obfuscate = function(arg){
+const obfuscate = function(arg) {
   let string = arg.toString();
   let newString = "";
-  for (let i = 0; i < string.length; i++){
-    switch(string[i]) {
-      case "a":
-        newString += "4";
-        break;
-      case "e":
-        newString += "3";
-        break;
-      case "o":
-        newString += "0";
-        break;
-      case "l":
-        newString += "1";
-        break;
-      default:
-        newString += string[i];
-        break;
+  for (let i = 0; i < string.length; i++) {
+    switch (string[i]) {
+    case "a":
+      newString += "4";
+      break;
+    case "e":
+      newString += "3";
+      break;
+    case "o":
+      newString += "0";
+      break;
+    case "l":
+      newString += "1";
+      break;
+    default:
+      newString += string[i];
+      break;
     }
   }
   return newString;
-}
+};
 
-console.log(obfuscate(arguments));
+console.log(obfuscate(args));
